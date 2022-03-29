ff651ac8848abf24428ed800c589cde67f9c2895
ryuto <ryu4to2program@gmail.com>
2022-03-29 09:14:43 +0900

    menu_nav調整

--- template_parts/mainMenu.php
+++ template_parts/mainMenu.php
@@ -10,7 +10,8 @@
                                 $tag_id = $tag->term_id;
                                 echo esc_url(get_tag_link($tag_id));
                             ?>" ontouchstart="">
-                            Staple<span>ご飯・麺</span>
+                            <span>Staple</span>
+                            <span>ご飯・麺</span>
                     </a>
                 </li>
                 <li class="p-menu-btn" data-aos="flip-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000" data-aos-delay="400">
@@ -19,7 +20,8 @@
                                 $tag_id = $tag->term_id;
                                 echo esc_url(get_tag_link($tag_id));
                             ?>" ontouchstart="">
-                            Main<span>メイン</span>
+                            <span>Main</span>
+                            <span>メイン</span>
                     </a>
                 </li>
                 <li class="p-menu-btn" data-aos="flip-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000" data-aos-delay="500">
@@ -27,7 +29,9 @@
                                 $tag = get_term_by('name', 'サイド', 'post_tag');
                                 $tag_id = $tag->term_id;
                                 echo esc_url(get_tag_link($tag_id));
-                            ?>" ontouchstart="">Side<span>サイド</span>
+                            ?>" ontouchstart="">
+                            <span>Side</span>
+                            <span>サイド</span>
                     </a>
                 </li>
                 <li class="p-menu-btn p-menu-btn__drink" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="600">
@@ -45,12 +49,14 @@
                                 $tag = get_term_by('name', 'スイーツ', 'post_tag');
                                 $tag_id = $tag->term_id;
                                 echo esc_url(get_tag_link($tag_id));
-                            ?>" ontouchstart="">Sweets<span>スイーツ</span>
+                            ?>" ontouchstart="">
+                            <span>Sweets</span>
+                            <span>スイーツ</span>
                     </a>
                 </li>
-                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="800"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '和風メニュー' ) ) ); ?>" ontouchstart="">Style<span>和風メニュー</span></a></li>
-                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="900"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '洋風メニュー' ) ) ); ?>" ontouchstart="">Style<span>洋風メニュー</span></a></li>
-                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="1000"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '中華風メニュー' ) ) ); ?>" ontouchstart="">Style<span>中華メニュー</span></a></li>
+                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="800"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '和風メニュー' ) ) ); ?>" ontouchstart=""><span>Style</span><span>和風メニュー</span></a></li>
+                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="900"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '洋風メニュー' ) ) ); ?>" ontouchstart=""><span>Style</span><span>洋風メニュー</span></a></li>
+                <li class="p-menu-btn" data-aos-anchor-placement="top-bottom" data-aos="flip-up" data-aos-duration="2000" data-aos-delay="1000"><a href="<?php echo esc_url( get_category_link( get_cat_ID( '中華風メニュー' ) ) ); ?>" ontouchstart=""><span>Style</span><span>中華メニュー</span></a></li>
             </ul>
         </div>
         <!-- /.p-menu-inner -->
