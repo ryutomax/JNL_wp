9c6f1541d690deceff9fc8a2945aa40730b88255
ryuto <ryu4to2program@gmail.com>
2022-04-06 08:51:12 +0900

    js

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,7 +1,6 @@
 // ========================================
 // ハンバーガーメニュー
 // ========================================
-
 $(".js-nav__btn").click(function () {//ボタンがクリックされたら
     $(this).toggleClass('is-btn__active');//ボタン自身に activeクラスを付与し
     $(".js-nav__open").toggleClass('is-menu__open');//ナビゲーションにpanelactiveクラスを付与
