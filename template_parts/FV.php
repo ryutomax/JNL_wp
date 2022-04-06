274b67272ca2d6ef9e7e9c976a9bbea0a95e3373
ryuto <ryu4to2program@gmail.com>
2022-04-06 14:34:47 +0900

    toriaezu

--- template_parts/FV.php
+++ template_parts/FV.php
@@ -1,32 +1,19 @@
 <!--
     FVのテンプレートです
 -->
-            <?php if(is_front_page() || is_404()): ?>
-            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/moringa.jpg'); ?>)">
+            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/fv_img.jpg'); ?>)">
                 <div class="p-fv-inner" data-aos="zoom-out" data-aos-duration="3000" data-aos-delay="1000">
                     <h2 class="p-fv-copy">
+                    <!-- TOPページ＆404ページ -->
+                    <?php if(is_front_page() || is_404()): ?>
                         <p>Moringa</p>
                         <p>Recipe</p>
-                    </h2>
-                </div>
-                <!-- /.p-fv-inner -->
-            </section>
-
-            <?php elseif(is_category() || is_tag()): ?>
-            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/moringa.jpg'); ?>)">
-                <div class="p-fv-inner" data-aos="zoom-out" data-aos-duration="3000" data-aos-delay="1000">
-                    <h2 class="p-fv-copy">
+                    <!-- カテゴリ・タグページ -->
+                    <?php elseif(is_category() || is_tag()): ?>
                         <p>Recipe List</p>
                         <p class="p-fv-copy_sub"><?php single_term_title( '', true ); ?></p>
-                    </h2>
-                </div>
-                <!-- /.p-fv-inner -->
-            </section>
-
-            <?php elseif(is_post_type_archive()): ?>
-            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/moringa.jpg'); ?>)">
-                <div class="p-fv-inner" data-aos="zoom-out" data-aos-duration="3000" data-aos-delay="1000">
-                    <h2 class="p-fv-copy">
+                    <!-- アーカイブページ -->
+                    <?php elseif(is_post_type_archive()): ?>
                         <?php if(is_post_type_archive( 'recipe' )): ?>
                         <p>Recipe List</p>
                         <p class="p-fv-copy_sub">レシピ一覧</p>
@@ -34,15 +21,8 @@
                         <p>News List</p>
                         <p class="p-fv-copy_sub">お知らせ一覧</p>
                         <?php endif; ?>
-                    </h2>
-                </div>
-                <!-- /.p-fv-inner -->
-            </section>
-
-            <?php elseif(is_page()): ?>
-            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/moringa.jpg'); ?>)">
-                <div class="p-fv-inner" data-aos="zoom-out" data-aos-duration="3000" data-aos-delay="1000">
-                    <h2 class="p-fv-copy">
+                    <!-- 固定ページ -->
+                    <?php elseif(is_page()): ?>
                         <?php if(is_page( array('contact','confirm','thanks'))): ?>
 
                         <p>
@@ -70,24 +50,16 @@
                         <p class="p-fv-copy_sub">サイトマップ</p>
 
                         <?php endif; ?>
-                    </h2>
-                </div>
-                <!-- /.p-fv-inner -->
-            </section>
-
-            <!-- カスタム投稿タイプの記事ページ -->
-            <?php elseif(is_singular()): ?>
-            <section class="p-fv"  style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/img/moringa.jpg'); ?>)">
-                <div class="p-fv-inner" data-aos="zoom-out" data-aos-duration="3000" data-aos-delay="1000">
-                    <h2 class="p-fv-copy">
+                    <!-- カスタム投稿タイプの記事ページ -->
+                    <?php elseif(is_singular()): ?>
                         <?php if(is_singular( 'recipe' )): ?>
                         <p>Recipe</p>
                         <?php elseif(is_singular( 'news' )):?>
                         <p>News</p>
                         <?php endif; ?>
+
+                    <?php endif; ?>
                     </h2>
                 </div>
                 <!-- /.p-fv-inner -->
             </section>
-
-            <?php endif; ?>
--- template_parts/banner.php
+++ template_parts/banner.php
@@ -3,7 +3,7 @@
 -->
 
             <section class="p-banner u-zindex__1000">
-                <a href="<?php echo esc_url('https://item.rakuten.co.jp/jn-lab/c/0000000129/'); ?>" class="p-banner-inner">
+                <a href="<?php echo esc_url('https://item.rakuten.co.jp/jn-lab/c/0000000129/'); ?>" class="p-banner-inner" target="_blank">
                 ご購入はこちら<img src="<?php echo esc_url(get_template_directory_uri() . '/img/arrow.PNG'); ?>" alt="arrow">
                 </a>
                 <!-- /.p-banner-inner -->
